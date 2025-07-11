0dda96172fd231f21332ead9cce4c9c1efa38071
dman6969 <dmaniac18@gmail.com>
2025-07-11 11:16:05 -0400

    Add symbol_map with core symbolic-action pairs

---
+++ files/home/axionet-agk-yesha/yesha_modules/core/symbol_map.js
@@ -0,0 +1,1 @@
+export const symbolMap = { "awaken": "initialize_protocol", "observe": "engage_sensors", "respond": "begin_response_chain", "remember": "log_memory_event", "evolve": "trigger_learning_cycle" };
