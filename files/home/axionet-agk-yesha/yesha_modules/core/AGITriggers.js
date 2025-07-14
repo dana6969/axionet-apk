fb9037318b298abbc2af0fe6f979f48c715d7692
dman6969 <dmaniac18@gmail.com>
2025-07-14 16:57:39 -0400

    Linked AGITrigger to symbolic modules: coreReasoner, logger, state tracker

--- files/home/axionet-agk-yesha/yesha_modules/core/AGITriggers.js
+++ files/home/axionet-agk-yesha/yesha_modules/core/AGITriggers.js
@@ -3,3 +3,4 @@ import { startAutonomyLoop } from "./autonomy_loop.js";
 
 memoryTrigger();
 startAutonomyLoop();
+import coreReasoner from "../symbolic/core_reasoner.js"; import { updateState, getState } from "../symbolic/state_tracker.js"; import { logSymbolInteraction } from "../symbolic/logger.js"; export function AGITrigger(symbols, context = "live") { updateState("context", context); logSymbolInteraction(symbols, getState("context")); return coreReasoner(symbols, { context: getState("context") }); }
